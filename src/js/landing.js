1c48e6b9929a8435f5e0f9f9e9be2f1686e9d95b
CuttingedgeDev <cuttingedgedev114@gmail.com>
2022-05-17 00:33:59 +0800

    modify styling

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,7 +1,11 @@
 function openNav() {
   document.getElementById('landingBurger').style.width = '100%';
+  document.getElementById('landing-burger-btn-group').style.display = 'flex';
 }
 
 function closeNav() {
   document.getElementById('landingBurger').style.width = '0%';
+  setTimeout(function() {
+    document.getElementById('landing-burger-btn-group').style.display = 'none';
+  }, 100);
 }
