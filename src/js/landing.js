773fafabf8c6c72449c87d6ca1598992fb3255e7
CuttingedgeDev <cuttingedgedev114@gmail.com>
2022-05-16 16:42:01 +0800

    coding burgermenu

---
+++ src/js/landing.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("landing-burger").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("landing-burger").style.width = "0%";
+}
