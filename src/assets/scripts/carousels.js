824426e110098aa6fb2219c1dfe68a4a69eb7b3c
CuttingedgeDev <cuttingedgedev114@gmail.com>
2022-10-25 17:08:31 +0800

    insert slick slide on homepage(multifamily)

---
+++ src/assets/scripts/carousels.js
@@ -0,0 +1,97 @@
+// event of resize dom
+var state = 0;
+
+// function resizeFunc() {
+//   var w = window.outerWidth;
+//   var h = window.outerHeight;
+//   if (w > 767 & state == 0) { 
+//     state = 1;
+//     // document.getElementById('marketingSlide').classList.remove('marketingSlide');
+    
+//     console.log("big");
+
+    
+//   } else if (w < 767 & state == 1) {
+//     state = 0;
+//     console.log("small");
+
+//     // document.getElementById('marketingSlide').classList.add('marketingSlide');
+
+
+//     $(document).ready(function(){
+//       $('.marketingSlide').slick({
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear',
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//       });
+//     });
+
+//     // $(document).ready(function(){
+//     //   $('.familierSlide').slick({
+
+//     //     autoplay: true,
+//     //     autoplaySpeed: 3000,
+//     //   });
+//     // });
+//   }
+// }
+
+// slide init
+if ( window.outerWidth > 767) {
+  // document.getElementById('marketingSlide').classList.remove('marketingSlide');
+
+} else {
+  // document.getElementById('marketingSlide').classList.add('marketingSlide');
+
+  $(document).ready(function(){
+    $('.marketingSlide').slick({
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+    });
+
+    // $('.familierSlide').slick({
+    //   infinite: true,
+    //   speed: 500,
+    //   fade: true,
+    //   cssEase: 'linear',
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 3000,
+    // });
+
+    // $('.familierSlide').each(function(num, elem) {
+    //   elem = $(elem);
+    //   elem.slick({
+    //     infinite: true,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //   });
+  
+    //   elem.next('.marketingSlide').slick({
+    //     infinite: true,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear',
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 3000,
+    //   });
+    // });
+
+  });
+
+}
+
